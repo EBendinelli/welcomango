284aa82f101b1f45e7abd203e2a28e33138012d0
eliotbendinelli <eliot.bendinelli@welcomango>
2015-06-13 19:02:00 +0200

    [Master] FOSUsers route & config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Welcomango\Bundle\CrmBundle\WelcomangoCrmBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
