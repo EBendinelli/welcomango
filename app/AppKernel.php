c86f012440306c175976f40f89af4a192b16f25c
jblanchon <JBaptiste05@gmail.com>
2015-06-14 12:41:33 +0200

    [master] Maj AppKernel / composer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,7 +29,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Genemu\Bundle\FormBundle\GenemuFormBundle(),
-            new Welcomango\Bundle\ExperienceBundle\WelcomangoExperienceBundle(),
             new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle()
         );
 
