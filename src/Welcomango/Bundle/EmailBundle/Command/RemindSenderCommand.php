83e2f9bf3b7beb6dae4b21e3b90a9c86f5981401
eliotbendinelli <eliot.bendinelli@gmail.com>
2016-04-06 15:59:44 +0200

    [Master] Changed the command to send a reminder to people with no experience. Must link it to a cron task. Added trans

--- src/Welcomango/Bundle/EmailBundle/Command/RemindSenderCommand.php
+++ src/Welcomango/Bundle/EmailBundle/Command/RemindSenderCommand.php
@@ -20,12 +20,14 @@ class RemindSenderCommand extends ContainerAwareCommand
         $this
             ->setName('welcomango:remind:sender')
             ->setDescription('Send email based on parameters')
-            ->addArgument('time', InputArgument::REQUIRED, 'desired created');
+            ->addArgument('user', InputArgument::OPTIONAL, 'desired user')
+            ->addArgument('time', InputArgument::OPTIONAL, 'desired created');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $time = $input->getArgument('time');
+        $specificUser = $input->getArgument('user');
 
         $counter             = 0;
         $mails               = array();
@@ -35,21 +37,40 @@ class RemindSenderCommand extends ContainerAwareCommand
         $doctrine = $this->getContainer()->get('doctrine');
         $logger   = $this->getContainer()->get('logger');
 
-        $users       = $doctrine->getRepository('Model:User')->findCreatedBefore($time);
-        $experiences = $doctrine->getRepository('Model:Experience')->findAll();
 
-        foreach ($experiences as $experience) {
-            $usersWithExperience[] = $experience->getCreator();
+        if($specificUser){
+            $users = $doctrine->getRepository('Model:User')->findById($specificUser);
+        }else {
+            if ($time) {
+                $users = $doctrine->getRepository('Model:User')->findCreatedBefore($time);
+            } else {
+                $users = $doctrine->getRepository('Model:User')->findAll();
+            }
+
+            $experiences = $doctrine->getRepository('Model:Experience')->findAll();
+            foreach ($experiences as $experience) {
+                $usersWithExperience[$experience->getCreator()->getId()] = $experience->getCreator();
+            }
+
+            $users = array_diff($users, $usersWithExperience);
         }
 
-        $users    = array_diff($users, $usersWithExperience);
         $progress = new ProgressBar($output, count($users));
-
+        $translator = $this->getContainer()->get('translator');
 
         foreach ($users as $user) {
-            $output->writeln("<comment>Sent to ".$user->getEmail()."</comment>");
+            $email = \Swift_Message::newInstance()
+                ->setSubject($translator->trans('email.experience.creation.reminder.subject', array(), 'interface'))
+                ->setFrom(['no-reply@welcomango.com' => 'Welcomango team'])
+                ->setTo($user->getEmail())
+                ->setBody($this->getContainer()->get('templating')->render('WelcomangoEmailBundle:EmailTemplate:experienceCreationReminder.html.twig', ['user' => $user, 'type' => 'Text']), 'text/plain')
+                ->addPart($this->getContainer()->get('templating')->render('WelcomangoEmailBundle:EmailTemplate:experienceCreationReminder.html.twig', ['user' => $user]), 'text/html')
+            ;
+            $mailer->send($email);
+
             $counter++;
             $progress->advance();
+            $output->writeln("\n<comment>Sent to ".$user->getEmail()."</comment>");
             $mails[] = $user->getEmail();
         }
 
